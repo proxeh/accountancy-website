7747e280f103887de8d384ab5abe125c6e2e6980
Jamie Shaw <proxeh@gmail.com>
2023-09-12 10:22:14 +0100

    More template updates

--- components/Layout/layout.js
+++ components/Layout/layout.js
@@ -1,11 +1,10 @@
-import { Box, Container } from "@mui/material";
 import NavBar from "./NavBar";
 import HeroImage from "./HeroImage";
 import Footer from "./Footer";
 
 export default function Layout({ children }) {
 	return (
-		<main style={{ backgroundColor: "#eee" }}>
+		<main>
 			<NavBar />
 			<HeroImage />
 			{children}
